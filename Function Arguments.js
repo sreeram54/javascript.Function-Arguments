4ef047c9d72a6061349e29bc74c0e21738ee75ca
sreeram54 <vaddisreeram54@gmail.com>
2022-08-17 15:33:41 +0530

    Function Arguments

---
+++ Function Arguments.js
@@ -0,0 +1,19 @@
+// function without parameter,  a function which make a number square
+function square() {
+    var  num = 2
+    var sq = num * num
+    console.log(sq)
+  }
+  
+  square() // 4
+  
+  // function without parameter
+  function addTwoNumbers() {
+    var numOne = 10
+    var numTwo = 20
+    var sum = numOne + numTwo
+  
+    console.log(sum)
+  }
+  
+  addTwoNumbers() // a function has to be called by its name to be executed 
